4f37613701c44482e3d35c42aa681281f09198b0
The Guy with The Hat <ryancarlson486@gmail.com>
2014-04-01 10:05:49 -0400

    Update UnicoinsHax.js
    Turned into userscript

--- Unicoins/UnicoinsHax.js
+++ Unicoins/UnicoinsHax.js
@@ -1,3 +1,20 @@
+// ==UserScript==
+// @name UnicoinHax
+// @namespace http://stackexchange.com
+// @description Constantly gives you Unicoins whenever you visit the main page for a Stack Exchange site
+// @include http://*.stackexchange.com
+// @include http://stackoverflow.com
+// @include http://*.stackoverflow.com
+// @include http://mathoverflow.com
+// @include http://serverfault.com
+// @include http://superuser.com
+// @include http://stackapps.com
+// @version 1
+// @grant none
+// ==/UserScript==
+
+//You can also just copy this code and paste it into the console on any page where the domain is a Stack Exchange site:
+
 var hackMeSomeUnicoins = function(myFkey) {
   console.log("Unicoin hacking begun!");
   window.setInterval(function() {
