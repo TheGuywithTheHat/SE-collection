83f3bdd1e73022370b241777df94eb57ed50005d
The Guy with The Hat <ryancarlson486@gmail.com>
2014-04-19 12:47:09 -0400

    Create ChatInputResizifizer.user.js

---
+++ customizations/ChatInputResizifizer.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Chat Input Resizifizer
+// @namespace   http://stackexchange.com
+// @description Various customizations for Stack Exchange chat
+// @match       http://chat.stackexchange.com/rooms/*
+// @match       http://chat.stackoverflow.com/rooms/*
+// @match       http://chat.meta.stackexchange.com/rooms/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// @require     http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
+// @resource    customCSS http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// ==/UserScript==
+
+var newCSS = GM_getResourceText("customCSS");
+GM_addStyle(newCSS);
+
+$('#input-area').resizable({
+    handles: 'n',
+    alsoResize: '#input'
+});
