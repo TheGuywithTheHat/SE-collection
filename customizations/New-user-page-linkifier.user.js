bc63e24dd0d65591e6e0bc2fd24c1af22414104e
The Guy with The Hat <ryancarlson486@gmail.com>
2014-05-09 08:31:06 -0400

    Added grant; changed name

--- customizations/New-user-page-linkifier.user.js
+++ customizations/New-user-page-linkifier.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name       New user page
-// @namespace  http://stackexchange.com/users/*
-// @version    1
+// @name         New User Page linkifier
+// @namespace    http://stackexchange.com/users/*
+// @version      1
 // @description  Links to the new user page
-// @match      http://stackoverflow.com/users/*
-// @copyright  2014+, The Guy with The Hat
+// @match        http://stackoverflow.com/users/*
+// @grant        none
+// @copyright    2014+, The Guy with The Hat
 // ==/UserScript==
 
 $(".gravatar > a[href*='http://stackoverflow.com/users/']").attr('href', $(".gravatar > a[href*='http://stackoverflow.com/users/']").attr('href').replace(new RegExp(/http:\/\/stackoverflow.com\/users\/(\d+).*/),'http://stackoverflow.com/users/reloaded/$1'))
