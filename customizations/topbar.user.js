868c80d47790182c562c1e7a5d08fc11c7e7c8dc
The Guy with The Hat <ryancarlson486@gmail.com>
2014-05-14 17:01:30 -0400

    Topbar fixed position

--- customizations/topbar.user.js
+++ customizations/topbar.user.js
@@ -16,6 +16,11 @@ $('.topbar').css('background', '-moz-linear-gradient(300deg, red, orange, yellow
 $('.topbar').css('background', '-webkit-gradient(linear, left center, right center, from(red), color-stop(14%, orange), color-stop(28%, yellow), color-stop(42%, green), color-stop(56%, blue), color-stop(70%, indigo), to(violet))');
 $('.topbar').css('background', '-o-linear-gradient(left, red, orange, yellow, green, blue, indigo, violet)');
 $('.topbar').css('background', '-ms-linear-gradient(left, red, orange, yellow, green, blue, indigo, violet)');
+$('.topbar').css('position', 'fixed');
+$('.topbar').css('top', '0px');
+$('.topbar').css('z-index', '100');
+
+$('body > div.container').css('margin-top', '34px');
 
 $('.topbar').children().css('background-color', 'transparent');
 
