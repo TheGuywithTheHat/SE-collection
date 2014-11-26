dd90ab240331176060ee65bc292bc1ddfd854413
The Guy with The Hat <ryancarlson486@gmail.com>
2014-11-26 16:15:25 -0500

    Update topbar.user.js

--- customizations/topbar.user.js
+++ customizations/topbar.user.js
@@ -2,20 +2,21 @@
 // @name        CustomSE
 // @namespace   http://stackexchange.com
 // @description A customization for Stack Exchange
-// @match       /^https?:\/\/(meta.)?askubuntu\.com/
-// @match       /^https?:\/\/(meta.)?mathoverflow\.net/
-// @match       /^https?:\/\/(meta.)?serverfault\.com/
-// @match       /^https?:\/\/stackapps\.com/
-// @match       /^https?:\/\/(?!chat).*\.stackexchange\.com/
-// @match       /^https?:\/\/(meta.)?stackoverflow\.com/
-// @match       /^https?:\/\/(meta.)?superuser\.com/
+// @match       http*://*.askubuntu.com/*
+// @match       http*://*.mathoverflow.net/*
+// @match       http*://*.serverfault.com/*
+// @match       http*://*.stackapps.com/*
+// @match       http*://*.stackexchange.com/*
+// @match       http*://*.stackoverflow.com/*
+// @match       http*://*.superuser.com/*
 // @grant       none
 // ==/UserScript==
 
-$('.topbar').css('background', '-moz-linear-gradient(300deg, red, orange, yellow, green, blue, indigo, violet)');
+/*$('.topbar').css('background', '-moz-linear-gradient(300deg, red, orange, yellow, green, blue, indigo, violet)');
 $('.topbar').css('background', '-webkit-gradient(linear, left center, right center, from(red), color-stop(14%, orange), color-stop(28%, yellow), color-stop(42%, green), color-stop(56%, blue), color-stop(70%, indigo), to(violet))');
 $('.topbar').css('background', '-o-linear-gradient(left, red, orange, yellow, green, blue, indigo, violet)');
-$('.topbar').css('background', '-ms-linear-gradient(left, red, orange, yellow, green, blue, indigo, violet)');
+$('.topbar').css('background', '-ms-linear-gradient(left, red, orange, yellow, green, blue, indigo, violet)');*/
+$('.topbar').css('background', 'green');
 $('.topbar').css('position', 'fixed');
 $('.topbar').css('top', '0px');
 $('.topbar').css('z-index', '100');
@@ -24,8 +25,8 @@ $('body > div.container').css('margin-top', '34px');
 
 $('.topbar').children().css('background-color', 'transparent');
 
-$('.network-items').css('background-color', 'rgba(255, 255, 255, 0.25)');
-$('.topbar-menu-links').css('background-color', 'rgba(255, 255, 255, 0.25)');
+$('.network-items').css('background-color', 'rgba(0, 0, 0, 0.25)');
+$('.topbar-menu-links').css('background-color', 'rgba(0, 0, 0, 0.25)');
 $('#search > input').css({
     'background-color': 'transparent',
     '-khtml-opacity': '.50',
@@ -34,4 +35,23 @@ $('#search > input').css({
     'filter': 'alpha(opacity=50)',
     'filter': 'progid:DXImageTransform.Microsoft.Alpha(opacity=0.5)',
     'opacity': '.50',
+});
+
+$(window).scroll(function(){
+    if($(window).scrollTop() > 34){
+        $('.topbar').css('opacity', '0.1');
+        $('.topbar').css('position', 'fixed');
+        $('body > div.container').css('margin-top', '34px');
+        $('.topbar').hover(function(){
+            $('.topbar').css('opacity', '1');
+        }, function(){
+            $('.topbar').css('opacity', '0.1');
+        });
+    }       
+    else {
+        $('.topbar').css('position', 'static');
+        $('.topbar').css('opacity', '1');
+        $('body > div.container').css('margin-top', '0px');
+        $('.topbar').hover(function(){}, function(){$('.topbar').css('opacity', '1');});
+    }
 });
