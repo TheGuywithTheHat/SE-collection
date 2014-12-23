34cb41b6e6ade5cca1a08744e1c9d9293357af7f
The Guy with The Hat <ryancarlson486@gmail.com>
2014-12-23 13:00:12 -0500

    better site matching

--- customizations/trains.user.js
+++ customizations/trains.user.js
@@ -2,15 +2,10 @@
 // @name        Trains
 // @namespace   http://stackexchange.com
 // @description Creates a button in chat that automatically duplicats the most recent message. Great for continuing trains.
-// @match       *://*.askubuntu.com/*
-// @match       *://*.mathoverflow.net/*
-// @match       *://*.onstartups.com/*
-// @match       *://*.serverfault.com/*
-// @match       *://*.stackapps.com/*
-// @match       *://*.stackexchange.com/*
-// @match       *://*.stackoverflow.com/*
-// @match       *://*.superuser.com/*
-// @version     1
+// @match       *://chat.stackexchange.com/rooms/*
+// @match       *://chat.meta.stackexchange.com/rooms/*
+// @match       *://chat.stackoverflow.com/rooms/*
+// @version     2
 // @grant       none
 // ==/UserScript==
 
